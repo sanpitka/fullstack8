24287b8c1faf42ca83c37c8a5f705a4ea0585b49
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-11-19 17:00:29 +0200

    8.2

--- index.js
+++ index.js
@@ -97,6 +97,14 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+  }
+
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
   }
 `
 
@@ -104,6 +112,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   }
 }
 
