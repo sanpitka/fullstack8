ad7ac6173a80929f3ad76fffe2b37ef116e63049
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-11-19 17:38:10 +0200

    8.4

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -118,13 +118,18 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter(book => book.author === args.author)
+    },  
     allAuthors: () => authors,
   },
   Author: {
     bookCount: (root) => {
       return books.filter(
-        book => book.author === root.name
+        book => b.author === root.name
       ).length
     }
   }
